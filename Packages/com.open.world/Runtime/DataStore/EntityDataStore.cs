5c9a6182c1a65aa4a62e5c0e44508dba1e5c2883
DESKTOP-P4LPPRA\Viachaslau <vyacheslav.bysel@gmail.com>
2024-07-23 19:02:01 +0200

    # 1.4.0 - Added system for saving map entity data to the data store.

---
+++ Packages/com.open.world/Runtime/DataStore/EntityDataStore.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace OpenWorld.DataStore
+{
+    public class EntityDataStore<T> : ScriptableObject, ISerializationCallbackReceiver where T : class
+    {
+        [SerializeField, HideInInspector]
+        private List<int> _entityIds;
+        [SerializeField, HideInInspector]
+        private List<T> _entityData;
+        private Dictionary<int, T> _data = new();
+
+        public T GetData(int entityID)
+        {
+            if (_data.TryGetValue(entityID, out T data))
+            {
+                return data;
+            }
+
+            return null;
+        }
+
+        public void AddOrUpdateData(int entityID, T data)
+        {
+            _data[entityID] = data;
+#if UNITY_EDITOR
+            EditorUtility.SetDirty(this);
+            AssetDatabase.SaveAssets();
+#endif
+        }
+
+        internal void RemoveData(int entityID)
+        {
+            if (_data.ContainsKey(entityID))
+            {
+                _data.Remove(entityID);
+            }
+        }
+
+        public void OnBeforeSerialize()
+        {
+            _entityIds.Clear();
+            _entityData.Clear();
+
+            foreach (var kvp in _data)
+            {
+                _entityIds.Add(kvp.Key);
+                _entityData.Add(kvp.Value);
+            }
+        }
+
+        public void OnAfterDeserialize()
+        {
+            _data = new Dictionary<int, T>();
+
+            for (int i = 0; i < _entityIds.Count && i < _entityData.Count; i++)
+            {
+                _data[_entityIds[i]] = _entityData[i];
+            }
+        }
+    }
+}
---
+++ Packages/com.open.world/Runtime/DataStore/MapEntityDataSync.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace OpenWorld.DataStore
+{
+    /// <summary>
+    /// Manages the lifecycle of map entity data, ensuring synchronization with the world data registry.
+    /// </summary>
+    /// <typeparam name="T">Type of world data.</typeparam>
+    [ExecuteInEditMode]
+    public class MapEntityDataSync<T> : MapEntityIdentifier where T : class, new()
+    {
+        [SerializeField]
+        private EntityDataStore<T> _worldDataRegistry;
+
+        protected override void OnIdentifierInitialize()
+        {
+            T worldData = _worldDataRegistry.GetData(ID);
+            if (worldData != null) LoadDataProperties(worldData);
+        }
+
+        protected virtual void LoadDataProperties(T data)
+        {
+        }
+
+        protected virtual void SaveDataProperties(ref T data)
+        {
+        }
+
+        protected override void OnIdentifierDestroy()
+        {
+            _worldDataRegistry.RemoveData(ID);
+        }
+
+        public void OnDestroy()
+        {
+            if (ID == 0) return;
+
+            T data = new T();
+            SaveDataProperties(ref data);
+            _worldDataRegistry.AddOrUpdateData(ID, data);
+        }
+    }
+}
